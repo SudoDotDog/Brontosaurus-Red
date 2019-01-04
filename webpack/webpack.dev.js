e673a01829b2fe8f1d2f5ecf14aaedb7068bd983
PCXPY <pc@wmpcxpy.com>
2019-01-04 15:21:57 -0600

    bump dependencys

--- webpack/webpack.dev.js
+++ webpack/webpack.dev.js
@@ -4,70 +4,22 @@
  * @description Development
  */
 
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const SudooWebpack = require("@sudoo/webpack-react").SudooWebpack;
 const path = require('path');
-const webpack = require('webpack');
 
 const BUILD_DIR = path.resolve(__dirname, '..', 'dist');
 const APP_DIR = path.resolve(__dirname, '..', 'src');
-const RENDERER_TSCONFIG_DIR = path.resolve(__dirname, '..', 'typescript', 'tsconfig.dev.json');
+const TSCONFIG_PATH = path.resolve(__dirname, '..', 'typescript', 'tsconfig.dev.json');
+const COMMON_SASS_DIR = path.resolve(__dirname, '..', 'style', 'common');
 
-const config = {
-    devtool: 'cheap-module-eval-source-map',
-    target: "web",
-    mode: "development",
-    entry: {
-        index: [
-            'react-hot-loader/patch',
-            'webpack-dev-server/client',
-            'webpack/hot/only-dev-server',
-            APP_DIR + "/index.tsx",
-        ],
-    },
-    output: {
-        filename: "[name].bundle.js",
-        path: BUILD_DIR,
-        publicPath: '/',
-    },
-    resolve: {
-        extensions: [".ts", ".tsx", ".js", ".json", ".css", ".sass"],
-    },
-    module: {
-        rules: [
-            require('./common/ts')(RENDERER_TSCONFIG_DIR),
-            ...require('./common/sass.dev'),
-            {
-                enforce: "pre",
-                test: /\.js$/,
-                loader: "source-map-loader",
-            },
-        ]
-    },
-    plugins: [
-        new webpack.WatchIgnorePlugin([/css\.d\.ts$/]),
-        new HtmlWebpackPlugin({
-            chunks: ['index'],
-            title: 'Brontosaurus',
-            template: require('./common/dirs').HTML_TEMPLATE_DIR,
-            filename: 'index.html',
-        }),
-        new webpack.DefinePlugin({
-            'process.env.NODE_ENV': JSON.stringify('development'),
-        }),
-        new webpack.LoaderOptionsPlugin({
-            debug: true
-        }),
-        new webpack.HotModuleReplacementPlugin(),
-        new webpack.NamedModulesPlugin(),
-    ],
-    devServer: {
-        hot: true,
-        contentBase: path.resolve(__dirname, 'app', 'renderer'),
-        publicPath: '/',
-        port: 8083,
-        inline: true,
-        historyApiFallback: true
-    },
-};
+module.exports = SudooWebpack.create({
+    APP_DIR,
+    BUILD_DIR,
+    COMMON_SASS_DIR,
 
-module.exports = config;
+    TSCONFIG_PATH,
+
+    APP_ENTRY_FILE_NAME: 'index.tsx',
+}, {
+    title: 'Brontosaurus',
+}).development();
