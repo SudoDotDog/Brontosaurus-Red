a5126f155c8c678aa07af6de8a2b35e471752181
WMXPY <wm@mengw.io>
2019-02-13 15:47:00 -0600

    implement me smart form

--- src/me/me.tsx
+++ src/me/me.tsx
@@ -29,13 +29,23 @@ export class Me extends React.Component<MeProp, MeState> {
         return (
             <NeonSmartForm
                 form={{
-                    password: INPUT_TYPE.PASSWORD,
-                    confirm: INPUT_TYPE.PASSWORD,
+                    password: {
+                        type: INPUT_TYPE.PASSWORD,
+                        display: 'Password',
+                    },
+                    confirm: {
+                        type: INPUT_TYPE.PASSWORD,
+                        display: 'Confirm Password',
+                    },
                 }}
                 title="Password Change"
                 submit="Update"
                 onSubmit={(result: any) => {
-                    console.log(result);
+                    if (result.password === result.confirm) {
+                        console.log(true);
+                    } else {
+                        console.log('error');
+                    }
                 }}
             />
         );
---
+++ src/me/repository/change-password.ts
@@ -0,0 +1,24 @@
+/**
+ * @author WMXPY
+ * @namespace Me_Repository
+ * @description Change Password
+ */
+
+import { Brontosaurus } from "@brontosaurus/web";
+import { Fetch } from "@sudoo/fetch";
+
+export const editPassword = async (password: string): Promise<string> => {
+
+    const response: {
+        account: string;
+    } = await Fetch
+        .post
+        .json('http://localhost:8080/account/edit/password')
+        .bearer(Brontosaurus.raw)
+        .add('username', Brontosaurus.token().username)
+        .add('password', password)
+        .fetch();
+
+    console.log(response);
+    return response.account;
+};
