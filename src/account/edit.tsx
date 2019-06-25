f34e6d6f508cfb0776563b9508e11dcec50ca2c0
WMXPY <wm@mengw.io>
2019-06-25 14:17:29 -0500

    add deactivte button

--- src/account/edit.tsx
+++ src/account/edit.tsx
@@ -14,6 +14,7 @@ import * as React from "react";
 import { RouteComponentProps } from "react-router-dom";
 import { fetchGroup, GroupResponse } from "../group/repository/group-fetch";
 import { editAccountAdminRepository } from "./repository/admin-edit";
+import { deactivateAccount } from "./repository/deactivate";
 import { singleFetchRepository, SingleFetchResponse } from "./repository/single-fetch";
 
 type UserEditProp = {
@@ -122,6 +123,15 @@ export class UserEdit extends React.Component<UserEditProp, UserEditState> {
                     removable
                     options={this.state.groups}
                 />
+
+                <NeonTitle size={SIZE.MEDIUM}>Dangerous</NeonTitle>
+                <NeonButton onClick={async () => {
+                    const isConfirm: boolean = window.confirm('Are you sure to deactivate "' + this.state.user.username + '"?');
+                    if (isConfirm) {
+                        await deactivateAccount(this.state.user.username);
+                        this.props.history.goBack();
+                    }
+                }}>Deactivate</NeonButton>
 
                 <NeonButton
                     size={SIZE.MEDIUM}
