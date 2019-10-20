43248976c13ee3cae4b051ff9ada3cb1c30e8d57
WMXPY <wm@mengw.io>
2019-10-20 12:35:31 -0500

    disable register subaccount

--- src/current/route.tsx
+++ src/current/route.tsx
@@ -14,7 +14,6 @@ export const CurrentRoute: React.FC = () => {
     return (
         <React.Fragment>
             <Route path="/current" exact component={CurrentMenu} />
-            <Route path="/current/register" component={CurrentRegister} />
         </React.Fragment>
     );
 };
