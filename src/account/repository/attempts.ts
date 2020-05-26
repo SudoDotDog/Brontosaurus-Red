fb34facc3b6c091ba4506cdb55792fc2277facde
WMXPY <wm@mengw.io>
2020-05-26 11:54:07 -0500

    implement account attempts

---
+++ src/account/repository/attempts.ts
@@ -0,0 +1,44 @@
+/**
+ * @author WMXPY
+ * @namespace Account_Repository
+ * @description Account Fetch Account
+ */
+
+import { Brontosaurus } from "@brontosaurus/web";
+import { Fetch } from "@sudoo/fetch";
+import { joinRoute } from "../../repository/route";
+
+export type AccountAttemptElement = {
+
+    readonly account: string,
+    readonly succeed: boolean,
+    readonly failedReason?: string,
+    readonly platform: string,
+    readonly userAgent: string,
+    readonly target: string,
+    readonly source: string,
+    readonly proxySources: string[],
+    readonly application: string,
+    readonly identifier: string,
+    readonly at: Date,
+};
+
+export type AccountAttemptResponse = {
+
+    readonly pages: string;
+    readonly attempts: AccountAttemptElement[];
+};
+
+export const fetchAccountAttempts = async (username: string, namespace: string, page: number): Promise<AccountAttemptResponse> => {
+
+    const response: AccountAttemptResponse = await Fetch
+        .post
+        .json(joinRoute('/account/attempts'))
+        .bearer(Brontosaurus.hard().raw)
+        .add('username', username)
+        .add('namespace', namespace)
+        .add('page', page)
+        .fetch();
+
+    return response;
+};
