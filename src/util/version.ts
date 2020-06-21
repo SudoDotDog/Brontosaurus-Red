913301bb8a0744c544d8370a93603b24836f204f
WMXPY <wm@mengw.io>
2020-06-20 19:02:28 -0500

    add get version function

---
+++ src/util/version.ts
@@ -0,0 +1,15 @@
+/**
+* @author WMXPY
+* @namespace Util
+* @description Version
+*/
+
+export const getVersion = (): string => {
+
+    if (process && process.env && process.env.RELEASE_VERSION) {
+        const path: string = process.env.RELEASE_VERSION;
+        return path;
+    }
+
+    return 'LOCAL';
+};
