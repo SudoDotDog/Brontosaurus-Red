292f634085e7ac1ac92e0a3d18cccafff4fc357e
WMXPY <wm@mengw.io>
2020-07-16 11:21:57 -0500

    fix all i18n relateds

--- src/navigation/index-nav.tsx
+++ src/navigation/index-nav.tsx
@@ -4,19 +4,38 @@
  * @description Index Navigation
  */
 
+import { SudooFormat } from "@sudoo/internationalization";
+import { Connector } from "@sudoo/redux";
 import * as React from "react";
 import { RouteComponentProps } from "react-router-dom";
+import { intl } from "../i18n/intl";
+import { PROFILE } from "../i18n/profile";
+import { IStore } from "../state/declare";
 import { SubButton } from "./sub-button";
 
-export const IndexNav: React.FC<RouteComponentProps> = (props: RouteComponentProps) => {
+type ConnectedStates = {
+
+    readonly language: SudooFormat;
+};
+
+const connector = Connector.create<IStore, ConnectedStates>()
+    .connectStates(({ preference }: IStore) => ({
+
+        language: intl.format(preference.language),
+    }));
+
+export type IndexNavProps = RouteComponentProps & ConnectedStates;
+
+export const IndexNavBase: React.FC<IndexNavProps> = (props: IndexNavProps) => {
 
     return (<React.Fragment>
-
         <SubButton
             selected={props.location.pathname === '/'}
             onClick={() => props.history.push('/')}
         >
-            Home
+            {props.language.get(PROFILE.HOME)}
         </SubButton>
     </React.Fragment>);
 };
+
+export const IndexNav: React.ComponentType = connector.connect(IndexNavBase);
