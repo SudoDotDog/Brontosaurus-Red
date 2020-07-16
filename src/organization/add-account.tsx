4cf7a08308d06774c862a20104b292dec0757edc
WMXPY <wm@mengw.io>
2020-07-16 11:09:39 -0500

    finish polish add account page

--- src/organization/add-account.tsx
+++ src/organization/add-account.tsx
@@ -6,24 +6,23 @@
 
 import { SudooFormat } from "@sudoo/internationalization";
 import { NeonButton } from "@sudoo/neon/button";
-import { MARGIN, SIZE } from "@sudoo/neon/declare";
+import { SIZE } from "@sudoo/neon/declare";
 import { NeonSticker, NeonStickerCut } from "@sudoo/neon/flag";
 import { NeonApplicable } from "@sudoo/neon/input";
 import { NeonIndicator } from "@sudoo/neon/spinner";
 import { NeonTable } from "@sudoo/neon/table";
-import { NeonTitle } from "@sudoo/neon/typography";
 import { Connector } from "@sudoo/redux";
 import * as React from "react";
 import { RouteComponentProps } from "react-router-dom";
 import { setOrganizationRepository } from "../account/repository/set-organization";
 import { fetchStandaloneAccount, FetchStandaloneAccountResponse, StandaloneAccountResponse } from "../account/repository/standalone-account-fetch";
 import { GoBack } from "../components/go-back";
+import { NamedTitle } from "../components/named-title";
 import { PageSelector } from "../components/page-selector";
 import { intl } from "../i18n/intl";
 import { PROFILE } from "../i18n/profile";
 import { IStore } from "../state/declare";
 import { createFailedCover, createSucceedCover } from "../util/cover";
-import { NamedTitle } from "../components/named-title";
 
 export type UserState = {
 
@@ -60,6 +59,11 @@ export class OrganizationAddAccountBase extends React.Component<UserProps, UserS
         page: 0,
     };
 
+    public componentDidMount() {
+
+        this._searchUser();
+    }
+
     public render() {
 
         return (
@@ -70,12 +74,11 @@ export class OrganizationAddAccountBase extends React.Component<UserProps, UserS
             >
                 <GoBack />
                 <NamedTitle about={this.props.language.get(
-                    PROFILE.MEMBER_OF_INSTANCE,
+                    PROFILE.ADD_ACCOUNT_TO_INSTANCE,
                     this.props.language.get(PROFILE.ORGANIZATION),
                 )}>
                     {this._getOrganizationName()}
                 </NamedTitle>
-                <NeonTitle margin={MARGIN.SMALL}>Add account to Organization: {this._getOrganizationName()}</NeonTitle>
                 <NeonApplicable
                     size={SIZE.MEDIUM}
                     apply={this.props.language.get(PROFILE.APPLY)}
@@ -84,15 +87,25 @@ export class OrganizationAddAccountBase extends React.Component<UserProps, UserS
                         this._searchUser();
                     })}
                 />
-
                 {this.state.users.length === 0
                     ? void 0
                     : <NeonTable
-                        headers={['Username', 'Groups', '2FA', 'Email', "Phone", 'Action']}
-                        style={{ marginTop: '1rem' }}>
+                        headers={[
+                            this.props.language.get(PROFILE.NAMESPACE),
+                            this.props.language.get(PROFILE.USERNAME),
+                            this.props.language.get(PROFILE.GROUPS),
+                            this.props.language.get(PROFILE.TWO_FACTOR_AUTHORIZATION),
+                            this.props.language.get(PROFILE.EMAIL),
+                            this.props.language.get(PROFILE.PHONE),
+                            this.props.language.get(PROFILE.ACTION),
+                        ]}
+                        style={{
+                            marginTop: '1rem',
+                        }}
+                    >
                         {this._renderUser()}
-                    </NeonTable>}
-
+                    </NeonTable>
+                }
                 <PageSelector
                     total={this.state.pages}
                     selected={this.state.page}
@@ -116,16 +129,22 @@ export class OrganizationAddAccountBase extends React.Component<UserProps, UserS
 
         return this.state.users.map((user: StandaloneAccountResponse) =>
             (<tr key={user.username}>
+                <td>{user.namespace}</td>
                 <td>{user.username}</td>
                 <td>{user.groups}</td>
-                <td>{Boolean(user.twoFA).toString()}</td>
+                <td>
+                    {Boolean(user.twoFA)
+                        ? this.props.language.get(PROFILE.YES)
+                        : this.props.language.get(PROFILE.NO)
+                    }
+                </td>
                 <td>{user.email}</td>
                 <td>{user.phone}</td>
                 <td>
                     <NeonButton
                         onClick={() => this._addTo(user.username, user.namespace)}
                         size={SIZE.RELATIVE}>
-                        Add To
+                        {this.props.language.get(PROFILE.ADD_TO)}
                     </NeonButton>
                 </td>
             </tr>),
@@ -137,38 +156,40 @@ export class OrganizationAddAccountBase extends React.Component<UserProps, UserS
         const organization: string = this._getOrganizationName();
         const validation: boolean = window.confirm(`Add "${username}" to "${organization}"?`);
 
+        if (!validation) {
+            return;
+        }
+
         this.setState({
             loading: true,
             cover: undefined,
         });
 
-        if (validation) {
-            try {
+        try {
 
-                const response = await setOrganizationRepository(username, namespace, organization);
+            const response = await setOrganizationRepository(username, namespace, organization);
 
-                this.setState({
-                    cover: createSucceedCover(
-                        this.props.language,
-                        response.account + ' > ' + response.organization,
-                        () => this.props.history.goBack(),
-                    ),
-                });
-            } catch (err) {
+            this.setState({
+                cover: createSucceedCover(
+                    this.props.language,
+                    response.account + ' > ' + response.organization,
+                    () => this.props.history.goBack(),
+                ),
+            });
+        } catch (err) {
 
-                this.setState({
-                    cover: createFailedCover(
-                        this.props.language,
-                        err.message,
-                        () => this.setState({ cover: undefined }),
-                    ),
-                });
-            } finally {
+            this.setState({
+                cover: createFailedCover(
+                    this.props.language,
+                    err.message,
+                    () => this.setState({ cover: undefined }),
+                ),
+            });
+        } finally {
 
-                this.setState({
-                    loading: false,
-                });
-            }
+            this.setState({
+                loading: false,
+            });
         }
     }
 
