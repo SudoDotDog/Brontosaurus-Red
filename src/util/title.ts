fb2b927a50d5744aec718aa4f35316034f37cbc5
WMXPY <wm@mengw.io>
2020-07-17 15:19:18 -0500

    add title instance manger

--- src/util/title.ts
+++ src/util/title.ts
@@ -8,11 +8,37 @@ import { Title } from "@sudoo/title";
 
 export class TitleManager {
 
-    private readonly _title: Title = Title.create();
+    private static readonly _instance: TitleManager = new TitleManager();
+
+    public static setInit(title: string): TitleManager {
+
+        const instance: TitleManager = this.instance;
+        instance.setInit(title);
+        return instance;
+    }
+
+    public static get instance(): TitleManager {
+
+        return this._instance;
+    }
+
+    private readonly _title: Title;
+
+    private constructor() {
+
+        this._title = Title.create();
+    }
 
     public setInit(title: string): this {
 
         this._title.setInit(title);
+        this._title.setLevelBase(`{} | ${title}`, 1);
+        return this;
+    }
+
+    public setSubPage(title: string): this {
+
+        this._title.setTitle(title);
         return this;
     }
 }
