cb7433fc4a990eacd70a8c338d446efb44a4c1eb
WMXPY <wm@mengw.io>
2020-07-24 09:43:11 -0500

    improve webpack lint

--- webpack/webpack.config.ts
+++ webpack/webpack.config.ts
@@ -7,10 +7,10 @@
 import { SudooWebpack } from "@sudoo/webpack-react";
 import * as Path from "path";
 
-const BUILD_DIR = Path.resolve(__dirname, '..', 'dist');
-const APP_DIR = Path.resolve(__dirname, '..', 'src');
-const TSCONFIG_PATH = Path.resolve(__dirname, '..', 'typescript', 'tsconfig.dev.json');
-const COMMON_SASS_DIR = Path.resolve(__dirname, '..', 'style', 'common');
+const BUILD_DIR: string = Path.resolve(__dirname, '..', 'dist');
+const APP_DIR: string = Path.resolve(__dirname, '..', 'src');
+const TSCONFIG_PATH: string = Path.resolve(__dirname, '..', 'typescript', 'tsconfig.dev.json');
+const COMMON_SASS_DIR: string = Path.resolve(__dirname, '..', 'style', 'common');
 
 export default SudooWebpack.create(
     {
--- webpack/webpack.dev.ts
+++ webpack/webpack.dev.ts
@@ -7,14 +7,14 @@
 import { SudooWebpack } from "@sudoo/webpack-react";
 import * as Path from "path";
 
-const BUILD_DIR = Path.resolve(__dirname, '..', 'dist');
-const APP_DIR = Path.resolve(__dirname, '..', 'src');
-const TSCONFIG_PATH = Path.resolve(__dirname, '..', 'typescript', 'tsconfig.dev.json');
-const COMMON_SASS_DIR = Path.resolve(__dirname, '..', 'style', 'common');
+const BUILD_DIR: string = Path.resolve(__dirname, '..', 'dist');
+const APP_DIR: string = Path.resolve(__dirname, '..', 'src');
+const TSCONFIG_PATH: string = Path.resolve(__dirname, '..', 'typescript', 'tsconfig.dev.json');
+const COMMON_SASS_DIR: string = Path.resolve(__dirname, '..', 'style', 'common');
 
 const PORT_NUMBER: number = 8082;
 
-module.exports = SudooWebpack.create(
+export default SudooWebpack.create(
     {
         APP_DIR,
         BUILD_DIR,
