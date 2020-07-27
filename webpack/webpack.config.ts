a93cf4896154b5f6349a7a267dfb7d0d402c661e
WMXPY <wm@mengw.io>
2020-07-27 09:28:33 -0500

    fix webpack production

--- webpack/webpack.config.ts
+++ webpack/webpack.config.ts
@@ -4,7 +4,7 @@
  * @description Production
  */
 
-import { SudooWebpack } from "@sudoo/webpack-react";
+import { SudooWebpackReact } from "@sudoo/webpack-react";
 import * as Path from "path";
 
 const BUILD_DIR: string = Path.resolve(__dirname, '..', 'dist');
@@ -12,15 +12,15 @@ const APP_DIR: string = Path.resolve(__dirname, '..', 'src');
 const TSCONFIG_PATH: string = Path.resolve(__dirname, '..', 'typescript', 'tsconfig.dev.json');
 const COMMON_SASS_DIR: string = Path.resolve(__dirname, '..', 'style', 'common');
 
-export default SudooWebpack.create({
+export default SudooWebpackReact.create({
 
-    APP_DIR,
-    BUILD_DIR,
-    COMMON_SASS_DIR,
+    applicationPath: APP_DIR,
+    applicationEntryFile: 'index.tsx',
 
-    TSCONFIG_PATH,
+    buildPath: BUILD_DIR,
+    commonSassPath: COMMON_SASS_DIR,
 
-    APP_ENTRY_FILE_NAME: 'index.tsx',
+    tsconfigPath: TSCONFIG_PATH,
 }, {
 
     defines: {
